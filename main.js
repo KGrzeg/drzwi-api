9d7eafb9614508b057d398bd15fab659857539f1
KGrzeg <kupczykgrzeg@gmail.com>
2022-04-01 20:10:23 +0200

    Fetch dynamic data

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const axios = require("axios");
 
 function tryToRequire(path, default_value) {
   try {
@@ -11,9 +12,10 @@ function tryToRequire(path, default_value) {
 }
 
 const settings = tryToRequire("./settings.json", {
-  ignored_hosts: []
+  ignored_hosts: [],
+  "api_url": "192.168.1.1",
+  "cookie": "Authorization=I%20am%20not%20a%20real%20cookie%2C%20you%20know"
 });
-
 
 const example_output = fs.readFileSync("example_request.txt", "utf-8");
 
@@ -49,6 +51,48 @@ function filterIgnoredDevices(devices) {
   return devices.filter(device => settings.ignored_hosts.indexOf(device.MAC) === -1);
 }
 
-const devices = readDHCPList(example_output);
+function fetchData() {
+  const url = `http://${settings.api_url}/userRpm/AssignedIpAddrListRpm.html`;
+  const Referer = `http://${settings.api_url}userRpm/MenuRpm.htm`;
+  const Cookie = settings.cookie;
 
-console.table(filterIgnoredDevices(devices));
+  return axios({
+    url,
+    method: "GET",
+    headers: {
+      Referer,
+      Cookie,
+      Connection: 'keep-alive',
+      Pragma: 'no-cache',
+      DNT: '1',
+      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+      'Cache-Control': 'no-cache',
+      'Upgrade-Insecure-Requests': '1',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) PotatoBrowser/69(DOS) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36',
+      'Accept-Language': 'en-US,en;q=0.9,pl-PL;q=0.8,pl;q=0.7',
+    }
+  })
+}
+
+async function getDevices() {
+  const response = await fetchData();
+  const { data } = response;
+  const devices = readDHCPList(data);
+
+  if (!devices)
+    return undefined;
+
+  return filterIgnoredDevices(devices);
+}
+
+async function main() {
+  const devices = await getDevices(example_output);
+
+  if (devices) {
+    console.table(devices);
+  } else {
+    console.log("Devices not found");
+  }
+}
+
+main();
